ec32d609844cb7ebdcc31437e3f747fd1e33a85d
Carina Wedengren <carina.wedengren@gmail.com>
2021-02-19 10:27:26 +0100

    Add Custom Exceptions for Unique ID

--- src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
+++ src/main/java/io/agileintelligence/ppmtool/services/ProjectService.java
@@ -1,9 +1,12 @@
 package io.agileintelligence.ppmtool.services;
 
 import io.agileintelligence.ppmtool.domain.Project;
+import io.agileintelligence.ppmtool.exceptions.ProjectIdException;
 import io.agileintelligence.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Locale;
 
 @Service
 public class ProjectService {
@@ -16,6 +19,13 @@ public class ProjectService {
 
         // Logic...
 
-        return projectRepository.save(project);
+        try {
+            project.setProjectIdentifier((project.getProjectIdentifier()).toUpperCase());
+            return projectRepository.save(project);
+        } catch (Exception e) {
+            throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase()+"' already exists");
+        }
+
+
     }
 }

aa0e9a1b6ee23bdd8faa1ffcd085924f05dcb090
Carina Wedengren <carina.wedengren@gmail.com>
2021-02-24 10:05:20 +0100

    Client - Add redux store

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,27 @@
 import './App.css';
+import React, { Component } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import "bootstrap/dist/css/bootstrap.min.css";
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import AddProject from './components/Project/AddProject';
+import store from './store';
 
-function App() {
-  return (
-    <Router>
-      <div className="App">
-        <Header />
-        <Route exact path="/dashboard" component={Dashboard} />
-        <Route exact path="/addProject" component={AddProject} />
-      </div>
-    </Router>
-  );
+class App extends Component {
+  render() {
+    return (
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Header />
+            <Route exact path="/dashboard" component={Dashboard} />
+            <Route exact path="/addProject" component={AddProject} />
+          </div>
+        </Router>
+      </Provider>
+    );
+  }
 }
 
 export default App;
